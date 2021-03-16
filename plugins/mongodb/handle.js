c1ee1334bdf16f656e4340f6ee52575f4103680f
Semih Çakmak <>
2021-03-16 09:51:40 +0300

    slqite upgraded.

--- plugins/mongodb/handle.js
+++ plugins/mongodb/handle.js
@@ -9,8 +9,8 @@ var dirs = util.dirs();
 var pluginHelper = require(`${dirs.core}pluginUtil`);
 var pluginMock = {
   slug: 'mongodb adapter',
-  dependencies: config.mongodb.dependencies
-}
+  dependencies: config.mongodb.dependencies,
+};
 
 // exit if plugin couldn't be loaded
 var cannotLoad = pluginHelper.cannotLoad(pluginMock);
@@ -22,8 +22,8 @@ var mode = util.gekkoMode();
 
 var collections = [
   mongoUtil.settings.historyCollection,
-  mongoUtil.settings.adviceCollection
-]
+  mongoUtil.settings.adviceCollection,
+];
 
 var connection = mongojs(config.mongodb.connectionString, collections);
 var collection = connection.collection(mongoUtil.settings.historyCollection);
@@ -31,17 +31,20 @@ var collection = connection.collection(mongoUtil.settings.historyCollection);
 if (mode === 'backtest') {
   var pair = mongoUtil.settings.pair.join('_');
 
-  collection.find({ pair }).toArray((err, docs) => { // check if we've got any records
+  collection.find({ pair }).toArray((err, docs) => {
+    // check if we've got any records
     if (err) {
       util.die(err);
     }
     if (docs.length === 0) {
-      util.die(`History table for ${config.watch.exchange} with pair ${pair} is empty.`);
+      util.die(
+        `History table for ${config.watch.exchange} with pair ${pair} is empty.`
+      );
     }
-  })
+  });
 }
 
-if(mongoUtil.settings.exchange) {
-    collection.createIndex({start: 1, pair: 1}, {unique: true}); // create unique index on "time" and "pair"
+if (mongoUtil.settings.exchange) {
+  collection.createIndex({ start: 1, pair: 1 }, { unique: true }); // create unique index on "time" and "pair"
 }
 module.exports = connection;
